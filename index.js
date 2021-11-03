d52d853962351a57ffd8fb2148d8902ff502e5c7
rikicop <ruperto1@protonmail.com>
2021-11-03 14:30:52 -0400

    validate pass

--- index.js
+++ index.js
@@ -6,21 +6,28 @@ app.use(express.json());
 const users = [
   {
     id: "1",
-    unsername: "jhon",
+    username: "jhon",
     password: "jhon123",
     isAdmin: true,
   },
   {
     id: "2",
-    unsername: "jame",
+    username: "jame",
     password: "jhon123",
     isAdmin: false,
   },
 ];
 
 app.post("/api/login", (req, res) => {
-  /* const { username, password } = req.body; */
-  res.json("Hey Funciona!!");
+  const { username, password } = req.body;
+  const user = users.find((u) => {
+    return u.username === username && u.password === password;
+  });
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(400).json("Username or password incorrect!");
+  }
 });
 
 app.listen(5000, () => console.log("El servidor está corriendo"));
